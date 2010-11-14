2b789108eb5cc339425173454fc3cdf01b404e82
Dustin Willis Webber <dustin.webber@gmail.com>
2010-11-14 11:25:44 -0500

    railtie_name is deprecated and has no effect.

--- lib/ezprint/railtie.rb
+++ lib/ezprint/railtie.rb
@@ -3,7 +3,6 @@ require 'action_dispatch/http/mime_types'
 require 'ezprint/pdf_helper'
 
 class Railtie < Rails::Railtie
-  railtie_name :ezprint
 
   initializer :init_mime_types do
     Mime::Type.register 'application/pdf', :pdf
